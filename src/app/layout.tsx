26e3bf28065956d78bbac1f3e2b2aaf65e1f7e6b
Georgy <pigargeorge@gmail.com>
2023-11-15 20:52:26 +0300

    add <Header>, <Footer> and content div container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,31 @@
 import React from "react";
 import StoreProvider from "store/StoreProvider";
-import { ApolloWrapper } from 'serverAPI/ApolloWrapper';
+import { Metadata } from "next";
+import Header from "components/header/Header";
+import Footer from "components/footer/Footer";
+import "../styles/global.css";
 
+export const metadata: Metadata = {
+  title: "Rick&Morty App",
+  description: "Helps you to find episodes",
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
+    <html lang='ru'>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <title>Rick&Morty App</title>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
       </head>
       <body suppressHydrationWarning={true}>
         <StoreProvider>
-            <ApolloWrapper>
-                {children}
-            </ApolloWrapper>
+          <Header />
+          <main>
+            <div className='container'>{children}</div>
+          </main>
+          <Footer />
         </StoreProvider>
       </body>
     </html>
