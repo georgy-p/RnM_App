7f49f4b783a3eeaba9d21f2da407c8a56015ff94
Georgy <pigargeorge@gmail.com>
2023-11-15 21:12:48 +0300

    add eslint-disabled

--- src/utils/sorters.ts
+++ src/utils/sorters.ts
@@ -4,7 +4,8 @@ export const sortByOldest = (episodes: Episode[]) =>
   episodes.sort((a, b) => {
     const aDate = new Date(a.air_date);
     const bDate = new Date(b.air_date);
-    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     return aDate - bDate;
   });
 
@@ -12,6 +13,7 @@ export const sortByNewest = (episodes: Episode[]) =>
   episodes.sort((a, b) => {
     const aDate = new Date(a.air_date);
     const bDate = new Date(b.air_date);
-    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     return bDate - aDate;
   });
