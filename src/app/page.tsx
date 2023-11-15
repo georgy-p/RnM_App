ef8a7dc0d76ae8be5a72b1eabd17b001614fe267
Georgy <pigargeorge@gmail.com>
2023-11-15 20:55:08 +0300

    delete Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
-import "../styles/global.css";
-import Image from "next/image";
-import logo from "../../public/logo.png";
 import Form from "components/form/Form";
 import { FC } from "react";
 import FoundEpisodes from "components/foundEpisodes/FoundEpisodes";
 
 const Page: FC = () => {
   return (
-    <div className="container">
-      <Image
-        priority={false}
-        src={logo}
-        alt="Rick and Morty logo"
-        width={300}
-        height={90}
-      />
+    <div>
       <Form />
       <FoundEpisodes />
     </div>
