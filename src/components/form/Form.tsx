e43bfd49190efa2ed1dac0cd9583155fe5be327c
Georgy <pigargeorge@gmail.com>
2023-11-15 20:50:49 +0300

    add store.setSubmitted call

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -11,13 +11,12 @@ import {
 } from "@mui/material";
 import "./styles.css";
 import React, { FC, FormEvent, useState } from "react";
-import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
 import { useStore } from "store/StoreProvider";
 import { observer } from "mobx-react-lite";
 
 const Form: FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
-  const [isNewest, setIsNewest] = useState<boolean>(true);
+  const [isNewest, setIsNewest] = useState<boolean>(false);
   const store = useStore();
 
   const handleSelect = () => setIsNewest(!isNewest);
@@ -25,6 +24,8 @@ const Form: FC = () => {
     e.preventDefault();
     const normalizedValue = inputValue.trim();
     store.setSearchValue(normalizedValue);
+    store.setSubmitted(true);
+    store.setNewest(isNewest);
     setInputValue("");
     setIsNewest(true);
   };
@@ -35,25 +36,25 @@ const Form: FC = () => {
 
   return (
     <div className={"form_container"}>
-      <Typography variant="h4">Find episodes</Typography>
+      <Typography variant='h4'>Find episodes</Typography>
       <form className={"form"} onSubmit={handleSubmit}>
         <TextField
           className={"input"}
           value={inputValue}
           inputRef={(input) => input && input.focus()}
-          placeholder="Enter the name of the episode"
+          placeholder='Enter the name of the episode'
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
             handleChange(event.target.value)
           }
         />
         <FormGroup className={"group"}>
-          <InputLabel id="demo-simple-select-label">Filtered by</InputLabel>
+          <InputLabel id='filter'>Filtered by</InputLabel>
           <Select value={isNewest ? 1 : 2} onChange={handleSelect}>
             <MenuItem value={1}>newest</MenuItem>
             <MenuItem value={2}>oldest</MenuItem>
           </Select>
         </FormGroup>
-        <Button type="submit" variant="contained" sx={{ height: "56px" }}>
+        <Button type='submit' variant='contained' sx={{ height: "56px" }}>
           Search
         </Button>
       </form>
